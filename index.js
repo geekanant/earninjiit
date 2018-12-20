d450073c1403cf14993f36724e4b6593c1ce7980
Anant Patni <anant.p@lucideustech.com>
2018-12-19 20:33:10 -0400

    Add mongoose

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
 var port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const mongoURI = "mongodb://xxxxx@ds145694.mlab.com:45694/earninjiit";
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 var Users = require("./routes/User");
 
