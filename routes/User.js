aa170120f4356c8707d7aa8d690a312c8901e8e4
Anant Patni <anant.p@lucideustech.com>
2018-12-25 12:30:10 -0400

    Add categories route

--- routes/User.js
+++ routes/User.js
@@ -37,6 +37,94 @@ users.post("/register", (req, res) => {
     });
 });
 
+users.get("/categories", (req, res) => {
+  const categories = [
+    {
+      id: "1",
+      name: "App Development",
+      title: "Need of an App Developer",
+    },
+    {
+      id: "2",
+      name: "Assignment Solving",
+      title: "Need someone to solve my assignment",
+    },
+    {
+      id: "3",
+      name: "Books",
+      title: "Need some books",
+    },
+    {
+      id: "4",
+      name: "Content Writing",
+      title: "Need a good content writer",
+    },
+    {
+      id: "5",
+      name: "Data Entry",
+      title: "Need a data entry person",
+    },
+    {
+      id: "6",
+      name: "Exam Help",
+      title: "Need Help for exams",
+    },
+    {
+      id: "7",
+      name: "Notes",
+      title: "Need Notes",
+    },
+    {
+      id: "8",
+      name: "Photoshop/Photo Editing",
+      title: "Need someone for photoshop work",
+    },
+
+    {
+      id: "9",
+      name: "Photoshoot",
+      title: "Need a photographer",
+    },
+    {
+      id: "10",
+      name: "Photocopy/Print Out",
+      title: "Need Print out of my lab assignments",
+    },
+    {
+      id: "11",
+      name: "Practical File",
+      title: "Need a Practical File",
+    },
+    {
+      id: "12",
+      name: "Promotions/Marketing",
+      title: "Need someone to promote my startup",
+    },
+    {
+      id: "13",
+      name: "Project",
+      title: "Need Help in my project",
+    },
+    {
+      id: "14",
+      name: "Video Editing",
+      title: "Need someone for Video Editing",
+    },
+    {
+      id: "15",
+      name: "Web Development",
+      title: "Need of a Website Developer",
+    },
+    {
+      id: "16",
+      name: "Other",
+    },
+  ];
+  res.send({
+    category: categories,
+  });
+});
+
 users.get("/posts", (req, res) => {
   Post.find(
     {
